cddc41046c2af2e5b3d99cf221444d7584bccd2e
dominik-hillmann <dominik.hillmann@gmx.de>
2023-03-11 23:58:34 +0100

    Add day 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,29 +1,70 @@
-// use std::env;
 use std::fs;
 use std::string::String;
 
 fn main() {
-  let file_path = String::from("./real-input.txt");
+    let file_path = String::from("./real-input.txt");
 
-  let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
+    let mut contents =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    contents = contents.replace("\r\n", "\n");
 
-  let bags = contents.split("\n\n");
+    let bags: Vec<&str> = contents.split("\n\n").collect();
 
-  let mut calory_counts: Vec<u32> = Vec::new();
-  for bag in bags {
-    let calories_in_bag: Vec<u32> = bag.lines()
-      .map(|calories_str| calories_str.parse::<u32>().unwrap())
-      .collect();
-    
-    let sum: u32 = calories_in_bag.iter().sum();
-    calory_counts.push(sum);
-  }
+    let calory_counts: Vec<u32> = to_int_calories(bags);
+    let largest = find_largest(calory_counts);
 
-  let mut largest: u32 = 0;
-  for count in calory_counts {
-    largest = largest.max(count);
-  }
+    println!("The largest bag of calories contains {} calories.", largest);
+}
 
-  println!("{}", largest);
+fn to_int_calories(bags: Vec<&str>) -> Vec<u32> {
+    let mut calory_counts: Vec<u32> = Vec::new();
+
+    for bag in bags {
+        let calories_in_bag: Vec<u32> = bag
+            .lines()
+            .map(|calories_str| calories_str.parse::<u32>().unwrap())
+            .collect();
+
+        let sum: u32 = calories_in_bag.iter().sum();
+        calory_counts.push(sum);
+    }
+
+    calory_counts
+}
+
+fn find_largest(calories: Vec<u32>) -> u32 {
+    let mut largest: u32 = 0;
+    for calory_value in calories {
+        largest = largest.max(calory_value);
+    }
+
+    largest
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{find_largest, to_int_calories};
+
+    #[test]
+    fn test_to_int_calories() {
+        let string_bags = vec!["1", "2", "3"];
+        let int_bags = to_int_calories(string_bags);
+
+        assert_eq!(int_bags, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test_find_largest() {
+        let actual_largest = 3;
+        let largest = find_largest(vec![1, 2, actual_largest]);
+
+        assert_eq!(largest, actual_largest);
+    }
+
+    #[test]
+    fn test_find_largest_on_empty() {
+        let largest = find_largest(vec![]);
+
+        assert_eq!(largest, 0);
+    }
 }
--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,41 +1,75 @@
-use std::str::FromStr;
+use std::fs;
+use std::string::String;
 
-fn main() {
-    println!("Hello, world!");
+enum Draw {
+  Rock,
+  Paper,
+    Scissors
 }
 
-enum MyDraw {
-    X,
-    Y,
-    Z
-}
-
-impl FromStr for MyDraw {
-
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<MyDraw, Self::Err> {
+impl Draw {
+    fn from(s: &str) -> Result<Draw, &str> {
         match s {
-            "X" => Ok(MyDraw::X),
-            "Y" => Ok(MyDraw::Y),
-            "Z" => Ok(MyDraw::Z),
-            _ => Err(()),
+            "A" => Ok(Draw::Rock),
+            "B" => Ok(Draw::Paper),
+            "C" => Ok(Draw::Scissors),
+
+            "X" => Ok(Draw::Rock),
+            "Y" => Ok(Draw::Paper),
+            "Z" => Ok(Draw::Scissors),
+
+            _ => Err(s)
         }
     }
 }
 
-enum TheirDraw {
-    A,
-    B,
-    C   
+fn main() {
+    let lines = read_lines("./sample-input.txt");
+
+    for line in lines.split("\n") {
+        let letters: Vec<&str> = line.split(" ").collect();
+        let [my_draw, their_draw] = <[&str; 2]>::try_from(letters).ok().unwrap();
+        print!("{}{} ", my_draw, their_draw);
+        /*
+        if let [my_draw, their_draw] = letters[0..=1] {
+            print!("{} {} - ", my_draw, their_draw);
+        } else {
+            panic!("Line does not follow two letter pattern.");
+        }*/
+        // let my_draw = letters[0];
+        // let their_draw = letters[1];
+
+        // print!("{} {}", my_draw, their_draw);
+    }
+}
+
+fn read_lines(filename: &str) -> String {
+    let file_path = String::from(filename);
+
+    let mut contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    contents = contents.replace("\r\n", "\n");
+
+    contents
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    
+    use std::io;
+    use crate::Draw;
+    use io::Result;
+
     #[test]
-    fn test_parser() {
-        assert_eq!(MyDraw::from_str("X"), MyDraw::X);
+    fn panics_on_illegal_letter() {
+        assert_eq!(Draw::from("A").is_ok(), true);
+        assert_eq!(Draw::from("Q").is_ok(), false);
+    }
+
+    #[test]
+    fn map_letter_to_draw() -> io::Result<()> {
+        match Draw::from("A") {
+            Ok(Draw::Rock) => Ok(()),
+            _ => Err(())
+        }
     }
 }
