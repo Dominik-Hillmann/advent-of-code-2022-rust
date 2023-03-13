fd179017c6f3bfec29c81656aa967ffb2ff69784
dominik-hillmann <dominik.hillmann@gmx.de>
2023-03-13 22:37:52 +0100

    hack at day 3

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,19 +1,28 @@
 use std::fs;
 
 fn main() {
+    let prio = match get_priority('c') {
+        Ok(prio) => prio,
+        Err(s) => panic!(s)
+    };
+
     println!("{}!", read_lines("sample-input.txt"));
     let text = read_lines("sample-input.txt");
     let lines: Vec<&str> = text.split("\n").collect();
 
-    for l in lines {
-        let line_length : usize = l.len();
-        /*const half_length: usize = line_length / 2;
-        let line_chars: Vec<char> = l.chars().collect();
+    for line in lines {
+        let res = get_halves(line);
+        let (compartment_1, compartment_2) = match res {
+            Ok((c1, c2)) => (c1, c2),
+            Err(s) => panic!("{}", s)
+        };
 
+        let common = find_common_item(compartment_1, compartment_2);
 
-        let first_half: [char; line_length / 2] = line_chars[0..(line_length / 2)];
-        let second_half: [char; line_length / 2]  = line_chars[(line_length / 2)..line_length];*/
-
+        match common {
+            Ok(common) => print!("{} has common {}.\n", line, common),
+            Err(s) => panic!("{}", s)
+        }
     }
 }
 
@@ -30,26 +39,98 @@ fn read_lines(filename: &str) -> String {
 fn get_halves(s: &str) -> Result<(Vec<char>, Vec<char>), &str> {
     let letters: Vec<char> = s.chars().collect();
     let length: usize = letters.len();
-    if length / 2 != 0 {
+
+    if length.rem_euclid(2) != 0 {
         return Err("The length of the input string must be divisible by 2.");
     }
 
+    let half_length = length / 2;
     let mut first_half: Vec<char> = Vec::new();
     let mut second_half: Vec<char> = Vec::new();
-    
 
-    Ok((vec!['a'], vec!['b']))
+    for i in 0..length {
+        if i < half_length {
+            first_half.push(letters[i]);
+        } else {
+            second_half.push(letters[i]);
+        }
+    }
+
+    Ok((first_half, second_half))
+}
+
+fn find_common_item(compartment_1: Vec<char>, compartment_2: Vec<char>)
+    -> Result<char, &'static str> {
+
+    for item in compartment_1 {
+        if compartment_2.contains(&item) {
+            return Ok(item.clone());
+        }
+    }
+
+    Err("Compartment 1 and compartment 2 do not share any items.")
+}
+
+fn get_priority(c: char) -> Result<u8, &'static str> {
+    let lower_case = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z'
+    ];
+    let upper_case = vec![
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z'
+    ];
+
+    return if !lower_case.contains(&c) && !upper_case.contains(&c) {
+        Err(&format!("{} is not a valid character.", c))
+    } else if lower_case.contains(&c) {
+        let index: Option<usize> =  lower_case.iter().position(|&letter| letter == c);
+        Ok(1 + index.try_into().unwrap())
+    } else {
+        Ok(26 + upper_case.iter().position(|&letter| letter == c).unwrap())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::vec;
-    use crate::get_halves;
+    use crate::{get_halves, find_common_item};
 
     #[test]
     fn test_get_halves_happy_path() {
-        let (first_half, second_half) = get_halves("abcd");
-        assert_eq!(first_half, vec!['a', 'b']);
-        assert_eq!(second_half, vec!['c', 'd']);
+        let res = get_halves("abcdef");
+
+        assert_eq!(res.is_ok(), true);
+
+        let (first_half, second_half) = res.unwrap();
+
+        assert_eq!(first_half, vec!['a', 'b', 'c']);
+        assert_eq!(second_half, vec!['d', 'e', 'f']);
+    }
+
+    #[test]
+    fn panics_on_uneven_length_input() {
+        let res = get_halves("aaaaa");
+
+        assert_eq!(res.is_ok(), false);
+    }
+
+    #[test]
+    fn test_find_common_item() {
+        let res = find_common_item(vec!['a', 'b'], vec!['a', 'c']);
+
+        assert_eq!(res.is_ok(), true);
+        assert_eq!(res.unwrap(), 'a');
+    }
+
+    #[test]
+    fn test_no_common_item() {
+        let res = find_common_item(vec!['a', 'b'], vec!['c', 'd']);
+
+        assert_eq!(res.is_ok(), false);
     }
 }
